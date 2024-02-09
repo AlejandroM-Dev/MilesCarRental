8a527ed5dfcca0a3e3f7813876a6fc9d1fa15162
AlejandroM-Dev <93066920+AlejandroM-Dev@users.noreply.github.com>
2024-02-09 14:47:06 -0500

    ConsultasMilesCar.sql

--- MilesCarRental/SQLQueryConsultasMilesCar.sql
+++ MilesCarRental/SQLQueryConsultasMilesCar.sql
@@ -1,11 +1,11 @@
 
---A. Obtener el número de personas que han consultado itinerarios con un valor mayor a mil dólares.
+--A. Obtener el nÃºmero de personas que han consultado itinerarios con un valor mayor a mil dÃ³lares.
 select count(*) from reservation where amount > 10000
---B. Identificar a las personas mayores de 40 años que viven en la ciudad de Bogotá y han realizado reservas.
+--B. Identificar a las personas mayores de 40 aÃ±os que viven en la ciudad de BogotÃ¡ y han realizado reservas.
 select distinct Client.Identification, FirstName, LastName, City, Age from Reservation
 left join Client on client.Identification = reservation.IdentificationClient 
 where Client.Age > 40
---C. Listar las reservas que se realizaron con pago mediante tarjeta de crédito.
+--C. Listar las reservas que se realizaron con pago mediante tarjeta de crÃ©dito.
 select * from Reservation where Payment = 'Tarjeta de credito'
 --D. Listar las reservas que se hicieron con pago en destino.
 select * from Reservation where Payment = 'Pago en destino'
